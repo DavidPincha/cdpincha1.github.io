dbc811d098b5169489f794193525aa63cca59bea
Cristian David Pincha Pilataxi <cdpincha1@espe.edu.ec>
2024-11-28 17:41:13 -0500

    Delete cdpincha1_Act02_3788 directory

--- cdpincha1_Act02_3788/main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('btnLimpiar').addEventListener('click', function() {
-    var table = document.querySelector('table tbody');
-    table.innerHTML = ''; // Limpia todas las filas de la tabla
-});
-
